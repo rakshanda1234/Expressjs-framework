d424d6d2109d34d4c6a30f93af587a0974bebd37
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-11-03 22:33:49 +0530

    task6 -project 1.add one more button on the navbar called Contact Us

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
-app.use("/shop", shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
